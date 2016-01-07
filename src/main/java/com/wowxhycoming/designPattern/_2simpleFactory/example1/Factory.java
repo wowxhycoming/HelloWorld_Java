5ddf491935cebbaa5f4fa20f8471ad12930b034f
wowxhycoming <wowxhycoming@sina.com>
2016-01-07 13:20:00 +0800

    增加GIF说明；调整结构

--- src/main/java/com/wowxhycoming/designPattern/_2simpleFactory/example1/Factory.java
+++ src/main/java/com/wowxhycoming/designPattern/_2simpleFactory/example1/Factory.java
@@ -10,11 +10,11 @@ public class Factory {
 	 * @return 创造好的Api对象
 	 */
 	public static Api createApi(int condition){
-		//应该根据某些条件去选择究竟创建哪一个具体的实现对象
-		//这些条件可以从外部传入，也可以从其它途径获取
-		//如果只有一个实现，可以省略条件，因为没有选择的必要
+		// 应该根据某些条件去选择究竟创建哪一个具体的实现对象
+		// 这些条件可以从外部传入，也可以从其它途径获取
+		// 如果只有一个实现，可以省略条件，因为没有选择的必要
 		
-		//示意使用条件
+		// 使用条件
 		Api api = null;
 		if(condition == 1){
 			api = new ImplA();
